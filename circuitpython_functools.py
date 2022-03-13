afd69759d3a5a8855f4e199d2eace0615a86e712
tekktrik <tekktrik@gmail.com>
2022-03-13 14:14:51 -0400

    Initial commit

---
+++ circuitpython_functools.py
@@ -0,0 +1,82 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2022 Alec Delaney
+#
+# SPDX-License-Identifier: MIT
+"""
+`circuitpython_functools`
+================================================================================
+
+A CircuitPython implementation of CPython's functools library
+
+
+* Author(s): Alec Delaney
+
+Implementation Notes
+--------------------
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+"""
+
+import gc
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/tekktrik/CircuitPython_functools.git"
+
+cache_record = {}
+
+class ObjectMark:
+    pass
+
+# class _HashedSeq:
+    
+#     def __init__(self, tup):
+#         self.tup = tup
+#         self.hashvalue = hash(tup)
+
+#     def __hash__(self):
+#         return self.hashvalue
+    
+
+def _make_key(args, kwargs, kwd_mark = (ObjectMark(), )):
+    key = tuple(args)
+    if kwargs:
+        key += kwd_mark
+        for item in kwargs.items():
+            key += tuple(item)
+    return hash(key)
+
+def cache(user_function):
+    """Unbounded cache"""
+    sentinel = object()
+    cache_dict = {}
+    cache_get = cache_dict.get
+    
+    def cache_wrapper(*args, **kwargs):
+        key = _make_key(args, kwargs)
+        #print("key: {}".format(key))
+        #print("key hash: {}", hash(key))
+        result = cache_get(key, sentinel)
+        if result is not sentinel:
+            print("result found")
+            return result
+        print("result not found")
+        result = user_function(*args, **kwargs)
+        cache_dict[key] = result
+        return result
+
+    print(dir(user_function))
+    print(user_function.__class__)
+    cache_record[user_function.__name__] = cache_dict
+
+    return cache_wrapper
+
+def clear_caches(*cache_names):
+    if cache_names:
+        [cache_contents.clear() for cache_name, cache_contents in cache_record.items() if cache_name in cache_names]
+    else:
+        [cache_contents.clear() for cache_contents in cache_record.values()]
+    gc.collect()
---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/functools_simpletest.py
+    :caption: examples/functools_simpletest.py
+    :linenos:
